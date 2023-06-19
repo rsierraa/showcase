4308c520784075c6a5efd721a979c5250d5192bb
Jmaciass1 <Jmaciass1@users.noreply.github.com>
2023-06-19 20:50:16 +0000

    deploy: 97803c2551a7d9c32355bea71b477b4167f76b87

---
+++ sketches/shaders/postmalone/postman1.js
@@ -0,0 +1,67 @@
+let image_pg, noiseWarp_pg, godrays_pg;
+let imageTexture, noiseWarpShader, godraysShader;
+let noiseScale, noiseStrength, godraysIntensity;
+
+function preload() {
+  
+    imageTexture = loadImage('/showcase/sketches/shaders/postmalone/post.jpg');
+    noiseWarpShader = readShader('/showcase/docs/Shaders/fragments/noiseWarpShader.frag', { varyings: Tree.texcoords2 });
+    godraysShader = readShader('/showcase/docs/Shaders/fragments/godraysShader.frag', { varyings: Tree.texcoords2 });
+  }
+
+  function setup() {
+    createCanvas(600, 600, WEBGL);
+    noiseWarp_pg = createGraphics(width, height, WEBGL);
+    noiseWarp_pg.colorMode(RGB, 1);
+    noiseWarp_pg.textureMode(NORMAL);
+    noiseWarp_pg.shader(noiseWarpShader);
+
+    godrays_pg = createGraphics(width, height, WEBGL);
+    grodays_pg.colorMode(RGB, 1);
+    grodays_pg.textureMode(NORMAL);
+    grodays_pg.shader(grodays_pg);
+  
+    noiseScale = createSlider(0.001, 0.1, 0.01, 0.001);
+    noiseScale.position(width - 120, 10);
+    noiseScale.style('width', '80px');
+    noiseScale.input(() => {
+      noiseWarpShader.setUniform('noiseScale', noiseScale.value());
+    });
+    noiseWarpShader.setUniform('noiseScale', noiseScale.value());
+  
+    noiseStrength = createSlider(0.1, 10, 2, 0.1);
+    noiseStrength.position(width - 120, 35);
+    noiseStrength.style('width', '80px');
+    noiseStrength.input(() => {
+      noiseWarpShader.setUniform('noiseStrength', noiseStrength.value());
+    });
+    noiseWarpShader.setUniform('noiseStrength', noiseStrength.value());
+  
+    godraysIntensity = createSlider(0.0, 1.0, 0.5, 0.01);
+    godraysIntensity.position(width - 120, 60);
+    godraysIntensity.style('width', '80px');
+    godraysIntensity.input(() => {
+      godraysShader.setUniform('godraysIntensity', godraysIntensity.value());
+    });
+    godraysShader.setUniform('godraysIntensity', godraysIntensity.value());
+  }
+
+function draw() {
+  image_pg.background(0);
+  image_pg.textureMode(NORMAL);
+  image_pg.shader();
+  image_pg.image(imageTexture, -width / 2, -height / 2, width, height);
+
+  noiseWarp_pg.shader(noiseWarpShader);
+  noiseWarpShader.setUniform('image', image_pg);
+  noiseWarpShader.setUniform('resolution', [width, height]);
+  noiseWarp_pg.rect(-width / 2, -height / 2, width, height);
+
+  godrays_pg.shader(godraysShader);
+  godraysShader.setUniform('image', noiseWarp_pg);
+  godraysShader.setUniform('resolution', [width, height]);
+  godrays_pg.rect(-width / 2, -height / 2, width, height);
+
+  // Display final result
+  image(godrays_pg, 0, 0);
+}
