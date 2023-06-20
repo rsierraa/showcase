41df9d1ff19ef4a96ff7f5cadd8be943bbe402eb
Jaime <jmaciass@unal.edu.co>
2023-06-19 20:43:11 -0500

    ..

--- content/sketches/shaders/postmalone/postman1.js
+++ content/sketches/shaders/postmalone/postman1.js
@@ -1,15 +1,19 @@
-let myShader;
+let myShader, pg;
 let imageTexture;
 
 function preload() {
-  myShader = loadShader('/showcase/docs/Shaders/fragments/shader.vert', '/showcase/docs/Shaders/fragments/shader.frag');
+  //myShader = loadShader('/showcase/docs/Shaders/fragments/shader.vert', '/showcase/docs/Shaders/fragments/shader.frag');
+  myShader = readShader('/showcase/docs/Shaders/fragments/shader.frag', { varyings: Tree.texcoords2 });
   imageTexture = loadImage('/showcase/sketches/shaders/postmalone/post.jpg');
 }
 
 function setup() {
   createCanvas(800, 600, WEBGL);
-  noStroke();
-  shader(myShader);
+
+  pg = createGraphics(width, height, WEBGL);
+  pg.colorMode(RGB, 1);
+  pg.textureMode(NORMAL);
+  pg.shader(myShader);
   myShader.setUniform('u_resolution', [width, height]);
   myShader.setUniform('u_mouse', [mouseX, mouseY]);
   myShader.setUniform('u_time', millis() / 1000.0);
@@ -17,6 +21,8 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  quad(-1, -1, 1, -1, 1, 1, -1, 1);
+  render(pg);
+  pg.quad(-1, 1, 1, 1, 1, -1, -1, -1);
+  // display the resulted buffer
+  image(pg, 0, 0);
 }
